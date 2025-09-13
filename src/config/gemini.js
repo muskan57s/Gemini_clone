0e072a9ded31d78e5752b9278801cff336392bc3
muskan <muskan.singh8582@gmail.com>
2025-09-13 18:25:09 +0530

    first commit

---
+++ src/config/gemini.js
@@ -0,0 +1,60 @@
+
+
+/// To run this code you need to install the following dependencies:
+// npm install @google/genai mime
+// npm install -D @types/node
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+}  from "@google/generative-ai";
+
+const MODEL_NAME = 'gemini-1.5-flash';
+const API_KEY = "AIzaSyBo7Z_myrWQV8g-qDz771Azfptq86kz2kw";
+
+async function runChat(prompt){
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME});
+
+  const generationConfig = {
+    temperature: 0.9,
+    topK :1,
+    topP: 1,
+    maxOutputTokens: 2048,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history:[
+
+    ],
+  });
+
+  const result = await chat.sendMessage(prompt);
+  const response = result.response;
+  console.log(response.text());
+  return response.text();
+}
+
+export default runChat;
